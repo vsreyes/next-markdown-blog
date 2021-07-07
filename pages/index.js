e87b04061dc1ccbdd1d057914981c640a39adeef
vsreyes <virgilio.s.reyes@gmail.com>
2021-07-07 17:03:11 +0200

    Slug setup

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import path from 'path';
+import matter from 'gray-matter';
 import Head from 'next/head';
 
 export default function Home({ posts }) {
@@ -15,9 +16,29 @@ export default function Home({ posts }) {
 }
 
 export async function getStaticProps() {
+  // Get files from the posts dir
   const files = fs.readdirSync(path.join('posts'));
 
-  console.log(files);
+  // Get slug and frontmatter from posts
+  const posts = files.map(filename => {
+    // Create slug
+    const slug = filename.replace('.md', '');
+
+    // Get frontmatter
+    const markdownWithMeta = fs.readFileSync(
+      path.join('posts', filename),
+      'utf-8'
+    );
+
+    const { data: frontmatter } = matter(markdownWithMeta);
+
+    return {
+      slug,
+      frontmatter,
+    };
+  });
+
+  console.log(posts);
 
   return {
     props: {
